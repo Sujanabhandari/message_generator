de8861bbefe6016e325ac6603b1b2a3a62e01145
Sujana Bhandari <sujana6589@gmail.com>
2022-07-11 13:00:05 +0200

    Nest the arrays inside of the object

--- script.js
+++ script.js
@@ -1,50 +1,21 @@
-// const nameFunction = (names) => {
-//   for (let i = 0; i < names.length - 1; i++) {
-//     document.querySelector(
-//       ".messages"
-//     ).innerHTML += `${names[i]} is here <br />`;
-//   }
-// };
-
-  
-
 const message_array = [
-   
-  "You are awesome",
-  "Nothing is impossible",
-  "The bad news is time flies",
-  "Life has got all those twists and turns",
-  "Keep your face always toward the sunshine, and shadows will fall behind you.",
-  "Life has got all those twists and turns. You've got to hold on tight and off you go.",
-  "You are never too old to set another goal or to dream a new dream.",
+  { author: "Sinclair Lewi", message: "The author says one character's definition of a classic is any book he'd heard of before he was thirty." },
+  { author: "Jack Kerouac", message: "The best teacher is experienced and not through someone's distorted point of view" },
+  { author: "Jack Kerouac", message: "One day I will find the right words, and they all will be simple."},
+  { author: "Jane Austen", message: "Laugh as much as you choose, but you will not laugh me out of my opinion.",},
+  { author: "Alfred Lord Tennyson", message: "Made weak by time and fate, but strong in will To strive, to seek, to find, and not to yield."},
+  { author: "J.K. Rowling", message: "It matters not what someone is born, but what they grow to be."},
+  { author: "William Shakespeare", message:"I will wear him; In my heart’s core, ay, in my heart of heart."},
+  
 ];
 
-
-const message_generator = () => {
+const message_generator = () => message_array.map(() =>{
+    console.log(message_array)
     //The Math.floor() function returns the largest integer less than or equal to a given number.
     let randomMessage = Math.floor(Math.random() * message_array.length);
-    return document.querySelector(
-              "#messages"
-            ).innerHTML = message_array[randomMessage];
-}
+    console.log(message_array.length)
+    return (document.querySelector("#messages").innerHTML = 
+      message_array[randomMessage].author + " : " + message_array[randomMessage].message);
+  }) 
 // message_generator()
-console.log(message_generator());
-
-
-
-
-// const nameFunction = (names) => {
-//     let result = '';
-//     let messageLength = message_array2.length
-//     console.log(messageLength[2]);
-//     for (let i = 0; i < messageLength; i++) {
-//         console.log(messageLength[i]);
-//         result += message_array2[i].charAt(Math.floor(Math.random() * messageLength));
-//   };
-//   return result
-// }
-
-// console.log(nameFunction(1));
- 
-
-// nameFunction(message_array);
+console.log(message_generator);
